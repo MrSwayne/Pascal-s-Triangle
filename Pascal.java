cb0e38cf7e52295f1b3beea2c0c86ee3731d8021
MrSwayne <a.swayne92@gmail.com>
2017-08-02 01:01:54 +0100

    Pascal's Triangle
    My attempt at Pascal's Triangle (up to 2^31 - 1th row)

---
+++ Pascal.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.math.*;
+public class Pascal
+{
+	public static void main(String[] args)
+	{
+		//Up to how many rows to calculate (not for the feint of heart)
+		int row = 900;
+
+		pascalsTriangle(row);
+	}
+
+	//Displays Pascal's Triangle
+	public static void pascalsTriangle(int row)
+	{
+		for(long i = 0;i < row;i++)
+		{
+			for(long j = 0;j <=  i; j++)
+				//Calculates i choose j; i being row number and j being element number. basically calculates each individual element on each row and prints it out
+				System.out.print(calculate(new BigInteger(Long.toString(i)), new BigInteger(Long.toString(j))) + " ");
+			System.out.println();
+		}
+	}
+
+	//NchooseR for 2 big integers
+	public static BigInteger calculate(BigInteger n, BigInteger r)
+	{
+		return ((fact(n)).divide((fact(r)).multiply(fact(n.subtract(r)))));
+	}
+
+	//calculates factorial
+	public static BigInteger fact(BigInteger n)
+	{
+		if 			(n.equals(new BigInteger("0")))		return new BigInteger("1");
+		else											return n.multiply(fact(n.subtract(new BigInteger("1"))));
+	}
+}
